ccfe6f29e48b6f911a96a267395c6fa01df9a36b
Craig J Smith <nctiggy@gmail.com>
2017-08-24 21:51:08 -0700

    update the script

--- script.sh
+++ script.sh
@@ -21,4 +21,10 @@ apt install -y docker-ce
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
 mkdir /var/lib/paclabs_ && cp /tmp/docker-compose.yml /var/lib/paclabs_
+mkdir -p keys/web keys/worker
+ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+cp ./keys/web/tsa_host_key.pub ./keys/worker
 CONCOURSE_EXTERNAL_URL=http://10.4.44.20:8080 docker-compose -f /var/lib/paclabs_/docker-compose.yml up -d
